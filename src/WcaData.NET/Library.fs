331e24deec2a7fdd4ae7903d22346efb678d0b9e
Joe Tremblay <jos.trem@gmail.com>
2020-07-03 11:11:54 -0500

    Add get user by id

--- src/WcaData.NET/Library.fs
+++ src/WcaData.NET/Library.fs
@@ -1,5 +1,14 @@
 ﻿namespace WcaData.NET
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open FSharp.Data
+
+module GET =
+
+    let baseUrl = "https://worldcubeassociation.org/api/v0/"
+
+    let user id =
+
+        async { 
+            let! json = Http.AsyncRequestString(baseUrl + "users/" + id)
+            return json 
+        }
--- src/wca-cli/Program.fs
+++ src/wca-cli/Program.fs
@@ -1,9 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿
 open System
 open WcaData.NET
 
 [<EntryPoint>]
 let main argv =
-    Say.hello "foo"
-    0 // return an integer exit code
+    GET.user "2008KORI02"
+    |> Async.RunSynchronously
+    |> printfn "%s"
+
+    0 
