59be4ba939d578f6be54670e894f897b27e8b3e2
Joe Tremblay <jos.trem@gmail.com>
2020-07-03 10:13:29 -0500

    initial solution

---
+++ src/WcaData.NET/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace WcaData.NET
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/wca-cli/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open WcaData.NET
+
+[<EntryPoint>]
+let main argv =
+    Say.hello "foo"
+    0 // return an integer exit code
