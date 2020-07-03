b1f66d1e6f32c07d850cda37f8763cea43546424
Joe Tremblay <jos.trem@gmail.com>
2020-07-03 11:54:03 -0500

    Add Argu for argument parsing

--- src/wca-cli/Program.fs
+++ src/wca-cli/Program.fs
@@ -2,10 +2,45 @@
 open System
 open WcaData.NET
 
+
+open Argu
+
+[<CliPrefix(CliPrefix.DoubleDash)>]
+[<NoAppSettings>]
+type WcaArguments =
+    | User of user:string
+    | Records
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | User _ -> "get use by WCA id"
+            | Records _ -> "get world records"
+
+
+module API = 
+
+    let getUser id =
+        GET.user id
+            |> Async.RunSynchronously
+            |> printfn "%s"    
+
+
 [<EntryPoint>]
 let main argv =
-    GET.user "2008KORI02"
-    |> Async.RunSynchronously
-    |> printfn "%s"
+
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let parser = ArgumentParser.Create<WcaArguments>(programName = "wca-cli", errorHandler = errorHandler)
+
+    let run (getStr:Async<string>) =
+        Async.RunSynchronously getStr
+        |> printfn "%s" 
+
+    match parser.ParseCommandLine argv with
+    | id when id.Contains(User) -> id.GetResult(User) |> GET.user |> run
+    | x when x.Contains(Records) -> printfn "%s" "get records"
+    | _ -> printfn "%s" (parser.PrintUsage())
+
+    //|> Option.map GET.user
 
     0 
